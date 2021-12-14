d947c1721c1be15137766ec4bbff5135f87c8f66
qwert21y <94437237+qwert21y@users.noreply.github.com>
2021-12-14 16:27:51 +0300

    Add files via upload

---
+++ Zadacha 14 Anikin/Zadacha 14 Anikin/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadacha_14_Anikin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int rast = Convert.ToInt32(textBox1.Text);
+            if (radioButton1.Checked == true)
+            { int sum = 100 * rast; }
+            else if (radioButton2.Checked)
+            { int sum = 200 * rast; }
+            textBox2.Text = Convert.ToString(rast);
+        }
+    }
+}
+    
+
