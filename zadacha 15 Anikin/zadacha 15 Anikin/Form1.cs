f2f5174a9256f2f12df329ea9dd9d456511dc627
qwert21y <94437237+qwert21y@users.noreply.github.com>
2021-12-16 14:26:04 +0300

    Add files via upload

---
+++ zadacha 15 Anikin/zadacha 15 Anikin/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zadacha_15_Anikin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                int k = Convert.ToInt32(textBox2.Text);
+                textBox1.Text = Convert.ToString(k * 10);
+            }
+            if (radioButton2.Checked)
+            {
+                int k = Convert.ToInt32(textBox2.Text);
+                textBox1.Text = Convert.ToString(k * 20);
+            }
+            if (radioButton3.Checked)
+            {
+                int k = Convert.ToInt32(textBox2.Text);
+                textBox1.Text = Convert.ToString(k * 30);
+            }
+        }
+    }
+}
+    
